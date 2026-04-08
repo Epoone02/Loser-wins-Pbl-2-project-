03978ed3a39ba1fae831157af18a855911ad37de
Azfaltz <raffael.raffiani3@gmail.com>
2026-04-08 12:06:21 +0200

    add databank

--- main.py
+++ main.py
@@ -1,60 +1,102 @@
-class Arbre:
+import csv
+import streamlit as st
 
-  def __init__(self, e, f1, f2):
-    self.donnee = e
-    self.filsG = f1
-    self.filsD = f2
+class Player:
+    def __init__(self, name, bid):
+        self.name = name
+        self.bid = int(bid)
+        self.right = None
+        self.left = None
 
-  def insert(self, valeur):
-    a = self
-    continuer = True
-    while continuer:
-      if valeur < a.donnee:
-        if a.filsG is None:
-          a.filsG = Arbre(valeur, None, None)
-          continuer = False
+    def is_leaf(self):
+        return self.right is None and self.left is None
+
+
+class Bid_tree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, name, bid):
+        bid = int(bid)
+        if self.root is None:
+            self.root = Player(name, bid)
+            return
+        current = self.root
+        while True:
+            if bid == current.bid:
+                self.root = self.delete(current.bid, self.root)                           
+                return
+            elif bid > current.bid:
+                if current.right is None:
+                    current.right = Player(name, bid)
+                    break
+                else:
+                    current = current.right
+            else:
+                if current.left is None:
+                    current.left = Player(name, bid)
+                    break
+                else:
+                    current = current.left
+
+    def build_tree(self, liste):
+        for item in liste:
+            self.insert(item[0], item[1])
+
+    def minBST(self):
+        if self.root is None:
+            return None
+        current = self.root
+        while current.left is not None:
+            current = current.left
+        return [current.name, current.bid]
+
+    def maxBST(self):
+        if self.root is None:
+            return None
+        current = self.root
+        while current.right is not None:
+            current = current.right
+        return [current.name, current.bid]
+
+    def delete(self, bid, node):          
+        if node is None:
+            return None
+        if bid < node.bid:
+            node.left = self.delete(bid, node.left)
+        elif bid > node.bid:
+            node.right = self.delete(bid, node.right)
         else:
-          a = a.filsG
-      else:
-        if a.filsD is None:
-          a.filsD = Arbre(valeur, None, None)
-          continuer = False
-        else:
-          a = a.filsD
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            else:
+                name, bid2, node.left = self.suppmax(node.left)
+                node.name, node.bid = name, bid2
+        return node
+
+    def suppmax(self, node):
+        if node.right is None:
+            return node.name, node.bid, node.left
+        name, bid, node.right = self.suppmax(node.right)
+        return name, bid, node
 
 
-def insertion(arbre, val):
-  arbre = arbre
-  continuer = True
-  while continuer:
-    if val < arbre.donnee:
-      if arbre.filsG is None:
-        arbre.filsG = Arbre(val, None, None)
-        continuer = False
-      else:
-        arbre = arbre.filsG
-    else:
-      if arbre.filsD is None:
-        arbre.filsD = Arbre(val, None, None)
-        continuer = False
-      else:
-        arbre = arbre.filsD
+def load_bid(file):
+    bid_list = []
+    with open(file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)
+        for row in reader:
+            if len(row) >= 2:
+                bid_list.append(row)
+    return bid_list
 
 
-def genereABR(liste):
-  arbre = Arbre(liste[0], None, None)
-  for valeur in liste[1:]:
-    insertion(arbre, valeur)
-  return arbre
+bid = load_bid('Loser-wins-Pbl-2-project-/APP_lowbid_data/lowbid_stress_200k.csv')
+tree1 = Bid_tree()
+tree1.build_tree(bid)
 
-def parcoursI(arbre):
-  if arbre is None:
-    return []
-  else:
-    return parcoursI(arbre.filsG) + [arbre.donnee] + parcoursI(arbre.filsD)
-  
-def minABR(arbre):
-  a = arbre
-  while a.filsG is not None:
-    a = a.filsG
-  return a.donnee
+
+
