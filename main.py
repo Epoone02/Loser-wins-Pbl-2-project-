e362ee132b8b39ca6203c5513407623f1bef981f
gab-esme <gabriel.assouline@esme.fr>
2026-04-08 12:03:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -57,3 +57,40 @@ def maxBST(self):
     while current.right is not None:
         current = current.right
     return [current.name, current.bid]
+def delete(self, bid, node):          
+        if node is None:
+            return None
+        if bid < node.bid:
+            node.left = self.delete(bid, node.left)
+        elif bid > node.bid:
+            node.right = self.delete(bid, node.right)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            else:
+                name, bid2, node.left = self.suppmax(node.left)
+                node.name, node.bid = name, bid2
+        return node
+def suppmax(self, node):
+        if node.right is None:
+            return node.name, node.bid, node.left
+        name, bid, node.right = self.suppmax(node.right)
+        return name, bid, node
+
+
+def load_bid(file):
+    bid_list = []
+    with open(file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)
+        for row in reader:
+            if len(row) >= 2:
+                bid_list.append(row)
+    return bid_list
+
+
+bid = load_bid('Loser-wins-Pbl-2-project-/APP_lowbid_data/lowbid_stress_200k.csv')
+tree1 = Bid_tree()
+tree1.build_tree(bid)
